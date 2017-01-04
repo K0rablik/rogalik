681717e21768ccbf7310e39913ceb49a2c7cb21e
k0rablik <alice.visokova@yandex.ru>
2017-01-04 20:48:02 +0700

    minor change in creature

--- creature.rb
+++ creature.rb
@@ -1,8 +1,9 @@
 class Creature < IngameObject
     def initialize(options={})
         super
-        @object[:room_pos][:y] = options[:mapy]
+        @object[:room_pos] = {}
         @object[:room_pos][:x] = options[:mapx]
+        @object[:room_pos][:y] = options[:mapy]
         @map = options[:map]
         @matrix = @map.rooms[0][0]
     end
